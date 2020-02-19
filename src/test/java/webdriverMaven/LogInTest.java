39d8aca040198710400e00e2a821e5f751dffbc2
Deepak <deepakdn444@gmail.com>
2020-02-19 12:58:56 +0530

    Intial commit

---
+++ src/test/java/webdriverMaven/LogInTest.java
@@ -0,0 +1,40 @@
+package webdriverMaven;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LogInTest {
+
+	public static WebDriver driver;
+
+	@BeforeSuite
+	public void setUp() {
+		WebDriverManager.firefoxdriver().setup();
+		driver = new FirefoxDriver();
+
+	}
+
+	@Test
+	public void doLogin() {
+		driver.get("http://www.gmail.com");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.findElement(By.id("identifierId")).sendKeys("deepakdn444@gmail.com");
+		driver.findElement(By
+				.xpath("/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/span/span"))
+				.click();
+	}
+
+	@AfterSuite
+	public void tearDown() {
+		driver.quit();
+	}
+}
